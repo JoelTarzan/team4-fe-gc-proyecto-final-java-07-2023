06c32cfdaf124e36bbcbcc88133c93f4a21be821
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-09-05 20:18:05 +0200

    Archivo BBDD y DTO User arreglado + IUserDAO comentado para posterior arreglo + UserController CRUD añadido

--- Aurum-RH/src/main/java/com/rest/aurumrh/dto/User.java
+++ Aurum-RH/src/main/java/com/rest/aurumrh/dto/User.java
@@ -31,6 +31,9 @@ public class User {
 
 	@Column(name = "lastname")
 	private String lastname;
+	
+	@Column(name = "email")
+	private String email;
 
 	@Column(name = "password")
 	private String password;
@@ -83,11 +86,12 @@ public class User {
 
 	}
 
-	public User(int id, String name, String lastname, String password, String description, int rating, String title,
+	public User(int id, String name, String lastname, String email, String password, String description, int rating, String title,
 			String phone, String web, String linkedin, String git, Blob avatar, Role role) {
 		this.id = id;
 		this.name = name;
 		this.lastname = lastname;
+		this.email = email;
 		this.password = password;
 		this.description = description;
 		this.rating = rating;
@@ -123,6 +127,14 @@ public class User {
 
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getPassword() {
@@ -252,9 +264,9 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", lastname=" + lastname + ", password=" + password
-				+ ", description=" + description + ", rating=" + rating + ", title=" + title + ", phone=" + phone
-				+ ", web=" + web + ", linkedin=" + linkedin + ", git=" + git + ", avatar=" + avatar + ", role=" + role
-				+ "]";
+		return "User [id=" + id + ", name=" + name + ", lastname=" + lastname + ", email=" + email + ", password="
+				+ password + ", description=" + description + ", rating=" + rating + ", title=" + title + ", phone="
+				+ phone + ", web=" + web + ", linkedin=" + linkedin + ", git=" + git + ", avatar=" + avatar + ", role="
+				+ role + "]";
 	}
 }
