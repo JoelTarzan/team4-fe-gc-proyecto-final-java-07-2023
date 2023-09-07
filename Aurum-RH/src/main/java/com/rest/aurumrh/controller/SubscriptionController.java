bc3c30576ea5c4b397e85781f8c1458f048f0aa5
Santbrau <marcosantoroserrano@gmail.com>
2023-09-07 18:57:37 +0200

    subscription customs

--- Aurum-RH/src/main/java/com/rest/aurumrh/controller/SubscriptionController.java
+++ Aurum-RH/src/main/java/com/rest/aurumrh/controller/SubscriptionController.java
@@ -11,14 +11,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rest.aurumrh.dto.OpenProcess;
 import com.rest.aurumrh.dto.Subscription;
+import com.rest.aurumrh.dto.User;
+import com.rest.aurumrh.service.OpenProcessServiceImpl;
 import com.rest.aurumrh.service.SubscriptionServiceImpl;
+import com.rest.aurumrh.service.UserServiceImpl;
 
 @RestController
 public class SubscriptionController {
 
 	@Autowired
 	SubscriptionServiceImpl subscriptionServiceImpl;
+	
+	@Autowired
+	OpenProcessServiceImpl openProcessServiceImpl;
+	
+	@Autowired
+	UserServiceImpl userServiceImpl;
 
 	@GetMapping("/subscriptions")
 	public List<Subscription> getAllSubscriptions() {
@@ -59,4 +69,21 @@ public class SubscriptionController {
 
 		subscriptionServiceImpl.deleteSubscription(id);
 	}
+	
+	@GetMapping("/subscriptions/open-process/{id}")
+	public List<Subscription> getAllSubscriptionsByOpenProcess(@PathVariable(name = "id") int id) {
+		
+		OpenProcess openProcess = openProcessServiceImpl.getOpenProcessById(id);
+		
+		return subscriptionServiceImpl.getAllSubscriptionsByOpenProcess(openProcess);
+	}
+	
+	@GetMapping("/subscriptions/user/{id}")
+	public List<Subscription> getAllSubscriptionsByUser(@PathVariable(name = "id") int id) {
+		
+		User user = userServiceImpl.getUserById(id);
+		
+		return subscriptionServiceImpl.getAllSubscriptionsByUser(user);
+	}
+	
 }
